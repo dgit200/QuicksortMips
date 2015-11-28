7c6aa39dceda8f7c92976a5cf44973f291a29fa2
dwightthomas <dwight.thomas@bison.howard.edugit config --global user.name dwightthomasgit config --global user.email dwight.thomas@bison.howard.edu>
2015-11-28 16:45:11 -0500

    Adding comments to the code

--- Quicksort/QuickSort/main.cpp
+++ Quicksort/QuickSort/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
 using namespace std;
-
+//Declaring the quicksort function
 void quickSort(int list[], int start, int end);
+
 int main()
-{
+{//The main where the code is run. This is used to test the quicksort function.
 	int arr[] = { 1, 12, 5, 26, 7, 14, 3, 7, 2 };
 	cout << "The lis is: \n";
 	for (int i = 0; i < 9; i++)
@@ -24,10 +25,12 @@ int main()
 }
 
 void quickSort(int list[], int start, int end)
-{
+{//This is the quicksort function defined.
+	//Setting up the varibales to used in the function
 	int a = start, b = end;
 	int swap_temp;
 	int pivot = list[(a + b) / 2];
+	//Do the partition of the array about the pivot(dividing the list into a lower list and upper list to be futher sorted.
 	while (a <= b) {
 		while (list[a] < pivot)
 			a++;
@@ -41,8 +44,11 @@ void quickSort(int list[], int start, int end)
 			b--;
 		}
 	};
+	//Calling the quicksort function recursivily to futher sort the partitions.
+	//This recursion is called to sort the lower partition
 	if (start < b)
 		quickSort(list, start, b);
+	//This recursion is called to sort the upper partition
 	if (a < end)
 		quickSort(list, a, end);
 }
