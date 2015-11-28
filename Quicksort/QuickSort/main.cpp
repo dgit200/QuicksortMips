20a960e91c7de4943a7cc9742829db41b6b8cfb0
dwightthomas <dwight.thomas@bison.howard.edugit config --global user.name dwightthomasgit config --global user.email dwight.thomas@bison.howard.edu>
2015-11-28 16:32:23 -0500

    Doing the partition for the sort

--- Quicksort/QuickSort/main.cpp
+++ Quicksort/QuickSort/main.cpp
@@ -13,4 +13,17 @@ void quickSort(int list[], int start, int end)
 	int a = start, b = end;
 	int swap_temp;
 	int pivot = list[(a + b) / 2];
+	while (a <= b) {
+		while (list[a] < pivot)
+			a++;
+		while (list[b] > pivot)
+			b--;
+		if (a <= b) {
+			swap_temp = list[a];
+			list[a] = list[b];
+			list[b] = swap_temp;
+			a++;
+			b--;
+		}
+	};
 }
