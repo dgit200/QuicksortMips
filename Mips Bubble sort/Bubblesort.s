4fe2d299808ce0ee7ddd68a18fb76ffde7ce7acb
dwightthomas <dwight.thomas@bison.howard.edugit config --global user.name dwightthomasgit config --global user.email dwight.thomas@bison.howard.edu>
2015-11-29 21:08:25 -0500

    Adding comments to the code

--- Mips Bubble sort/Bubblesort.s
+++ Mips Bubble sort/Bubblesort.s
@@ -12,66 +12,66 @@ comma: .asciiz ", "
 .globl main
 main:
 
-	la $a2, list
-	li $a1, 24
-	jal bubblesort
+	la $a2, list					# Setting array to $a2
+	li $a1, 24						# Setting the size to $a1
+	jal bubblesort					# Calling the bubble sort function
 	
 	
-	li $t0, 0
-	move $t1, $a2
-	printloop:
-	beq $t0, $a1, end
-	li 	$v0, 1
+	li $t0, 0								# Setting Variables for printing the array
+	move $t1, $a2							# Setting Variables for printing the array	
+	printloop:								# Calling the print loop
+	beq $t0, $a1, end						# while (n < size)
+	li 	$v0, 1								# print integer
 	lw $a0 ($t1)
 	syscall
-	li $v0, 4
+	li $v0, 4								# print comma in between integers
 	la $a0, comma
 	syscall
 	addi $t0, $t0, 1
 	addi $t1, $t1, 4
 	j printloop
 
-end:
+end:										# End the program
 	li $v0, 10
 	syscall
 
 
 
-bubblesort:
-	addi $t0, $t0, 0
+bubblesort:									# The bubble sort function
+	addi $t0, $t0, 0						# Setting up variables needed for nested loops
 	move $t1, $a1
-	addi $t3, $a1, -1
-	firstloop:
-		beq $t0, $t1, endsort  # while (i < size)
-		li $t2, 0	# j = 0	
+	addi $t3, $a1, -1						# a = size - 1
+	firstloop:								# The outer loop
+		beq $t0, $t1, endsort				# while (i < a)
+		li $t2, 0							# j = 0	
 
-		innerloop:
-			beq $t2 $t3, endinnerloop # while (j < k)
-			lw $t4, ($a2)	# $t4 = list[j]
-			addi $a2, $a2, 4
-			lw $t5, ($a2)	# $t5 = list[j + 1]
-			addi $a2, $a2, -4
-			slt $t6, $t4, $t5	# if($t4 < $t5)
-			beq $t6, 1, else	# do this
-			sw $t5 ($a2)	# this is the swap
-			addi $a2, $a2, 4
-			sw $t4 ($a2)	# this is the swap
-			addi $a2, $a2, -4
+		innerloop:							# the inner loop
+			beq $t2 $t3, endinnerloop		# while (j < k)
+			lw $t4, ($a2)					# $t4 = list[j]
+			addi $a2, $a2, 4				# getting list[j + 1]
+			lw $t5, ($a2)					# $t5 = list[j + 1]
+			addi $a2, $a2, -4				# Making list back to just j
+			slt $t6, $t4, $t5				# if($t4 < $t5)
+			beq $t6, 1, else				# do this
+			sw $t5 ($a2)					# this is the swap
+			addi $a2, $a2, 4				# this is the swap
+			sw $t4 ($a2)					# this is the swap
+			addi $a2, $a2, -4				# this is the swap
 
-			else:	# else go here
-			addi $a2, $a2, 4	# increment list
-			addi $t2, $t2, 1	# j++
-			j innerloop
+			else:							# else go here
+			addi $a2, $a2, 4				# increment list
+			addi $t2, $t2, 1				# j++
+			j innerloop						# jump back to innerloop
 
 		endinnerloop:
-			addi $t3, $t3 -1	# k--
-			li $t7 -4
-			mult $t3 $t7
-			mflo $t7
-			add $a2, $a2, $t7	# reset array to base
-			addi $a2, $a2, -4
-			addi $t0, $t0, 1	# i++
-			j firstloop	
+			addi $t3, $t3 -1				# k--
+			li $t7 -4						# Variable used to reset array
+			mult $t3 $t7					# Variable used to reset array
+			mflo $t7						# Variable used to reset array
+			add $a2, $a2, $t7				# reset array to base
+			addi $a2, $a2, -4				# reset array to base
+			addi $t0, $t0, 1				# i++
+			j firstloop						# jump back to outer loop
 
-endsort:			
-	jr $ra
+endsort:									# End the bublesort function			
+	jr $ra									# jump and return to the main
