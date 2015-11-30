576a24cf2703fcb6bd1bb01d9446fc9b35b21d02
Angel Mitchell <angel.mitchell@bison.howard.edu>
2015-11-29 22:55:49 -0500

    print initial array

--- QuickSort.s
+++ QuickSort.s
@@ -1,2 +1,33 @@
 #Quick sort algorithm
 
+.data
+array: .word 1, 12, 5, 26, 7, 14, 3, 8, 2
+space: .asciiz " "
+newline: .asciiz "\n"
+
+main:
+  la $t0, array		# load array address
+  addi $s1, $s1, 0	# s1 = start index
+  addi $s2, $s2, 8	# s2 = end index
+
+  move $s2, $a0
+  add $t1, $0, $0
+print:
+  li $v0, 1
+  lw $a0, 0($t0)
+  addi $t0, $t0, 4
+  addi $t1, $t1, 1
+  syscall
+  li $v0, 4
+  la $a0, space 
+  syscall
+  
+  bne $t1, $a1, print	# while(t1 != size)
+  li $v0, 4
+  la $a0, newline
+  syscall
+  
+done:
+  li $v0, 10
+  syscall
+
