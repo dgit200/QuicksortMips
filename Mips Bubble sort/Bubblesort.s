2b7ceae5cf1a70d218c81282a0ae385fd180e5df
dwightthomas <dwight.thomas@bison.howard.edugit config --global user.name dwightthomasgit config --global user.email dwight.thomas@bison.howard.edu>
2015-11-29 20:53:47 -0500

    Adding the nested loop of bubblesort function

--- Mips Bubble sort/Bubblesort.s
+++ Mips Bubble sort/Bubblesort.s
@@ -29,5 +29,33 @@ bubblesort:
 		beq $t0, $t1, endsort  # while (i < size)
 		li $t2, 0	# j = 0	
 
+		innerloop:
+			beq $t2 $t3, endinnerloop # while (j < k)
+			lw $t4, ($a2)	# $t4 = list[j]
+			addi $a2, $a2, 4
+			lw $t5, ($a2)	# $t5 = list[j + 1]
+			addi $a2, $a2, -4
+			slt $t6, $t4, $t5	# if($t4 < $t5)
+			beq $t6, 1, else	# do this
+			sw $t5 ($a2)	# this is the swap
+			addi $a2, $a2, 4
+			sw $t4 ($a2)	# this is the swap
+			addi $a2, $a2, -4
+
+			else:	# else go here
+			addi $a2, $a2, 4	# increment list
+			addi $t2, $t2, 1	# j++
+			j innerloop
+
+		endinnerloop:
+			addi $t3, $t3 -1	# k--
+			li $t7 -4
+			mult $t3 $t7
+			mflo $t7
+			add $a2, $a2, $t7	# reset array to base
+			addi $a2, $a2, -4
+			addi $t0, $t0, 1	# i++
+			j firstloop	
+
 endsort:			
 	jr $ra
