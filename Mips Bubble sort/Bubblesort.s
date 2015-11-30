2965942937beab7c1b392b50937a1f6f0fbc61f3
dwightthomas <dwight.thomas@bison.howard.edugit config --global user.name dwightthomasgit config --global user.email dwight.thomas@bison.howard.edu>
2015-11-29 20:50:14 -0500

    Setting up variables and main

---
+++ Mips Bubble sort/Bubblesort.s
@@ -0,0 +1,17 @@
+# Bubble Sort
+# Computer Organiztion Project
+# Dwight Thomas, Alanna Walton, James Walker, Angel
+
+.data
+# the array that holds unordered data
+list: .word 54, 23, 56, 32, 99, 7, 4, 2, 88, 9, 11, 21, 39, 55, 100, 101, 43, 1, 3, 69, -5, -24, -17, 0
+space: .asciiz " "
+comma: .asciiz ", "
+
+.text
+.globl main
+main:
+
+end:
+	li $v0, 10
+	syscall
